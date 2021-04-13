d48c7f57ea6d65d2f3a1b092fad935387095a5d7
kmsazidhasan <63607685+kmsazidhasan@users.noreply.github.com>
2021-04-13 21:35:02 +0600

    added a rectangle in draw file

---
+++ draw.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+import numpy as np
+
+frame = np.zeros((500, 500, 3), dtype = np.uint8)
+frame[:] = 0, 0, 255    # broadcasting
+cv.rectangle(frame, (0, 0), (frame.shape[1]//2, frame.shape[0]//2 ), (0, 255, 0), thickness = cv.FILLED)  # args -> frame, diag initial point, diag end point, line thickness = (-1) alternate 
+cv.imshow('Frame', frame)
+cv.waitKey(0)
