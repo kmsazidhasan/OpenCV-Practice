c270a76868402a49252985e2cbe215a45ef82740
kmsazidhasan <63607685+kmsazidhasan@users.noreply.github.com>
2021-04-14 04:27:04 +0600

    drawn a line

--- draw.py
+++ draw.py
@@ -3,10 +3,10 @@ import numpy as np
 
 frame = np.zeros((500, 500, 3), dtype = np.uint8)
 frame[:] = 0, 0, 255    # broadcasting
-#cv.rectangle(frame, (0, 0), (frame.shape[1]//2, frame.shape[0]//2 ), (0, 255, 0), thickness = cv.FILLED)  # args -> frame, diag initial point, diag end point, line thickness = (-1) alternate 
-cv.imshow('Frame', frame)
-cv.waitKey(0)
+cv.rectangle(frame, (0, 0), (frame.shape[1]//2, frame.shape[0]//2 ), (0, 255, 0), thickness = cv.FILLED)  # args -> frame, diag initial point, diag end point, line thickness = (-1) alternate 
+cv.imshow('Rectangle', frame)
 cv.circle(frame, (frame.shape[1]//2, frame.shape[0]//2 ), 50, (255, 0, 0), thickness = -1)
 cv.imshow('Circle', frame)
+cv.line(frame, (0, 0), (frame.shape[1]//2, frame.shape[0]//2 ), color=(255, 255, 255), thickness=3)
+cv.imshow('Line', frame)
 cv.waitKey(0)
-
