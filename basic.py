75ac4af4ee23b361dd8ec5bf8862b5081a14e815
kmsazidhasan <63607685+kmsazidhasan@users.noreply.github.com>
2021-04-14 06:44:10 +0600

    blurred an image with Gaussian blur

--- basic.py
+++ basic.py
@@ -9,4 +9,8 @@ img = rescaleFrame(cv.imread(img_path), 0.5)
 gray = cv.cvtColor(img, code= cv.COLOR_BGR2GRAY)
 
 cv.imshow('Gray', gray)
+
+# Blurring an Image
+blur = cv.GaussianBlur(img, ksize=(5,5), sigmaX=cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
 cv.waitKey(0)
