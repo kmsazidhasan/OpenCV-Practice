cffe9c6e728fbfec864cd085349670a926d66936
kmsazidhasan <63607685+kmsazidhasan@users.noreply.github.com>
2021-04-15 23:59:52 +0600

    adding a translation function

--- helper.py
+++ helper.py
@@ -1,7 +1,12 @@
 import cv2 as cv
+import numpy as np
 
 def rescaleFrame(frame, scale = 0.75):
     height = int(frame.shape[0] * scale)
     width = int(frame.shape[1] * scale)
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA) 
+
+def tranlate(img, x, y):
+    transMat = np.float32([[1, 0, x], [0, 1, y]])
+    
