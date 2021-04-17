ef986268317b155232b2529098774092730a33d1
kmsazidhasan <63607685+kmsazidhasan@users.noreply.github.com>
2021-04-17 23:57:26 +0600

    added rotate function

--- helper.py
+++ helper.py
@@ -11,3 +11,14 @@ def translate(img, x, y):
     transMat = np.float32([[1, 0, x], [0, 1, y]])
     dimensions = img.shape[1::-1]
     return cv.warpAffine(src = img, M = transMat, dsize = dimensions)
+
+def rotate(img, angle, rotPoint = None ):
+    height, width = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
