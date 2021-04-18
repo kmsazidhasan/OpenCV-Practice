68abd0357cb4fdb9e44948cd6d3651d6d698f671
kmsazidhasan <63607685+kmsazidhasan@users.noreply.github.com>
2021-04-18 23:47:31 +0600

    flipped an image

--- transformation.py
+++ transformation.py
@@ -5,9 +5,13 @@ from helper import *
 img = cv.imread('Photos/mosque_day.jpg') # returns a matrix 
 rescaledImg = rescaleFrame(img, 0.5)
 
-translated = translate(rescaledImg, 100, 100)
-
 rotated = rotate(rescaledImg, 45)
 cv.imshow('Rotated', rotated)
+
+translated = translate(rescaledImg, 100, 100)
 cv.imshow('Translated', translated)
+
+flip = cv.flip(rescaledImg, -1)
+cv.imshow('Flip', flip)
+
 cv.waitKey(0)
