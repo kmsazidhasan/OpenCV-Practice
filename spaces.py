cf346e1767d185424ddbce72e55a81dc37882f52
kmsazidhasan <63607685+kmsazidhasan@users.noreply.github.com>
2021-04-20 23:58:57 +0600

    converted am image to hsv format (hue saturation value)

--- spaces.py
+++ spaces.py
@@ -11,4 +11,8 @@ cv.imshow('Mosque', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
+
 cv.waitKey(0)
